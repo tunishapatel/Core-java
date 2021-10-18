3dca216527102b7678d169676cdb42bf8fc79549
Tanisha Patel <taishakabariya15@gamil.com>
2021-10-18 12:31:10 +0530

    The use of while loop and do while loop

---
+++ MynewProject/src/loop.java
@@ -0,0 +1,22 @@
+public class loop {
+    public static void main(String args[]){
+        System.out.println("for loop");
+        for(int x=1; x<11; x++){
+            System.out.println(x);
+        }
+        System.out.println("while loop");
+        int y=1;
+        while(y<20)
+        {
+        System.out.println(y);
+        y++;
+        }
+        System.out.println("do wile loop");
+
+        do{
+            System.out.println(y);
+            y++;
+        }while(y<30);
+
+    }
+}
