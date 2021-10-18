70156693d41170e142ad6d2a8471b07ea95d0ac8
Tanisha Patel <taishakabariya15@gamil.com>
2021-10-18 18:41:18 +0530

    The example of constructor

--- MynewProject/src/Construct.java
+++ MynewProject/src/Construct.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Construct {
+class Construct {
+    int i,j;
+    Construct(){
+        i=1;
+        j=2;
+
+    }
+    public static void main(String args[]){
+        Construct obj = new Construct();
+        System.out.println(" " +obj.i);
+        System.out.println(" " +obj.j);
+
+    }
 }
+
