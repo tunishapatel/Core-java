bfd6b9abd4c8a8e5159cf7759f84e9791bf45dc2
Tanisha Patel <taishakabariya15@gamil.com>
2021-10-18 18:42:32 +0530

    Example of Parameterized constructor

--- MynewProject/src/C2.java
+++ MynewProject/src/C2.java
@@ -1,6 +1,24 @@
-public class Checkage {
-    public static void main(String args[]){
-        int age = 40;
+class C2 {
 
-    }
+
+        int i,j,Sum;
+        C2(){
+            i = 1;
+            j = 5;
+            Sum = i + j;
+            System.out.println( " i= " +i);
+            System.out.println( " j= " +j);
+            System.out.println( " Sum= " +Sum);
+        }
+        void display(){
+            System.out.println(i+ " " +j+ " " +Sum);
+        }
+        public static void main(String[] args){
+
+            C2 obj = new C2();
+            obj.display();
+
+        }
+
 }
+
