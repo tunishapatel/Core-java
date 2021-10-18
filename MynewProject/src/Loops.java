98f7073cb26c36bf814a4e1e8322a7a24ef233ea
Tanisha Patel <taishakabariya15@gamil.com>
2021-10-18 16:46:59 +0530

    the ‘for’ loop

---
+++ MynewProject/src/Loops.java
@@ -0,0 +1,15 @@
+
+    public class Loops {
+        public static void main(String args[]) {
+
+            int i, j, row = 6;
+            for (i = 0; i < row; i++) {
+
+                for (j = 1; j <= i; j++) {
+                    System.out.print("* ");
+
+                }
+                System.out.println();
+            }
+        }
+}
