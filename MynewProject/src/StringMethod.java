51548134de597266c8006e8bcc19c079dfee13b3
Tanisha Patel <taishakabariya15@gamil.com>
2021-10-19 16:00:15 +0530

    String method program of all the string methods like charAt(), concat(), equals() etc.

---
+++ MynewProject/src/StringMethod.java
@@ -0,0 +1,22 @@
+public class StringMethod {
+  public static void main(String arg[]){
+      String targetString = "Learn Java is fun";
+      String s1 = "JAVA";
+      String s2 = "Java";
+      String s3 = "Hello Java";
+
+      System.out.println("Char at index 2(third position: " +targetString.charAt(2));
+      System.out.println("Concat: " + targetString.concat("-JAVA-"));
+      System.out.println("Checking equals ignoring case: " +s2.equalsIgnoreCase(s1));
+      System.out.println("Checking equals with case: " + s2.equals(s1));
+      System.out.println("Checking length: " + targetString.length());
+      System.out.println("Replacing function: " +targetString.replace("fun", "easy"));
+      System.out.println("Substring: " +targetString.substring(8));
+      System.out.println("Substring: " +targetString.substring(8,12));
+      System.out.println("Converting to lowercase: " + targetString.toLowerCase());
+      System.out.println("Converting to Uppercase: " + targetString.toUpperCase());
+      System.out.println("Trimming string: " + s3.trim());
+      System.out.println("Searching s1 in targetString: " + targetString.contains(s1) );
+      System.out.println("Searching s2 in targetString: " + targetString.contains(s2));
+  }
+}
