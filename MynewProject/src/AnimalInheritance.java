86ee2389ea84ca4dfa09fb16b7adc156434b004f
Tanisha Patel <taishakabariya15@gamil.com>
2021-10-20 17:33:09 +0530

    Single Inheritance example

---
+++ MynewProject/src/AnimalInheritance.java
@@ -0,0 +1,17 @@
+public class AnimalInheritance {
+
+    void eat() {
+        System.out.println("eating...");
+    }
+}
+    class Dog extends AnimalInheritance{
+        void bark(){System.out.println("barking...");}
+    }
+    class  TestInheritance{
+        public static void main(String[] args){
+            Dog d = new Dog();
+            d.bark();
+            d.eat();
+        }
+    }
+
