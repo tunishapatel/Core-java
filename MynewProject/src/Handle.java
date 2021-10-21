35c8f2c637a9fd8b870473c2787d25bf27b92a6f
Tanisha Patel <taishakabariya15@gamil.com>
2021-10-21 19:18:28 +0530

    A program to handle exceptions using try, catch, finally, throw, and throws keywords.

---
+++ MynewProject/src/Handle.java
@@ -0,0 +1,38 @@
+class Handle{
+
+    void div(int a, int b)throws ArithmeticException
+    {
+        if (b==0){
+            throw new ArithmeticException();
+        }
+        else {
+            int c;
+            c = a/b;
+            System.out.println(c);
+        }
+    }
+
+    public static void main(String args[]){
+        Handle h = new Handle();
+
+        try{
+            h.div(20,0);
+        }
+
+        catch (ArithmeticException a) {
+            System.out.println("Can't divide");
+            int x=11,y=0,z;
+            z = x*y;
+            System.out.println(z);
+        }
+
+        finally {
+            int p=3,q=1,r;
+            r = p + q;
+            System.out.println(r);
+            System.out.println("Result");
+        }
+
+        System.out.println("Operation ended");
+    }
+}
