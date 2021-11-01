c9b1d5365b1fffef77956c2c93a1b99f86aee2cd
Tanisha Patel <taishakabariya15@gamil.com>
2021-11-01 11:35:52 +0530

    A program to create a text file

---
+++ MynewProject/src/Createfile.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+
+public class Createfile {
+
+    public static void main(String[] args) {
+
+        try {
+
+            File myObj = new File("myTestFile.docx");
+
+            if (myObj.createNewFile()) {
+
+                System.out.println("File created: " + myObj.getName());
+
+            } else {
+
+                System.out.println("File already exists.");
+
+            }
+        } catch (IOException e) {
+
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+
+        }
+    }
+}
