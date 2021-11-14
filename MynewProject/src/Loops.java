15943792a6d2c4103cb2cedc8a6c002e4ea93d7f
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 23:16:55 +0530

    Update Loops.java
    Corrected formatting

--- MynewProject/src/Loops.java
+++ MynewProject/src/Loops.java
@@ -1,15 +1,14 @@
+public class Loops {
+    public static void main(String args[]) {
+// Create for loop
+        int i, j, row = 6;
+        for (i = 0; i < row; i++) {
 
-    public class Loops {
-        public static void main(String args[]) {
+            for (j = 1; j <= i; j++) {
+                System.out.print("* ");
 
-            int i, j, row = 6;
-            for (i = 0; i < row; i++) {
-
-                for (j = 1; j <= i; j++) {
-                    System.out.print("* ");
-
-                }
-                System.out.println();
             }
+            System.out.println();
         }
+    }
 }
