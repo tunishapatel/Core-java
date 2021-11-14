c8f9e6b9d386f27bc45ab469da92cc041769a5b6
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 22:39:50 +0530

    Update Arrays.java
    Corrected formatting

--- MynewProject/src/Arrays.java
+++ MynewProject/src/Arrays.java
@@ -1,19 +1,24 @@
 public class Arrays {
-    public static void main(String[] args){
-        double[] myFirstArray = {0.0, 1.2, 2.4, 3.6, 8.8};
+    public static void main(String[] args) {
+        double[] myFirstArray = {
+            0.0,
+            1.2,
+            2.4,
+            3.6,
+            8.8
+        };
         System.out.println(myFirstArray.length);
-
-        for(int i=0; i<myFirstArray.length; i++) {
+        for (int i = 0; i < myFirstArray.length; i++) {
             System.out.println(myFirstArray[i]);
         }
-
+// sum the array 
         double sum = 0;
-        for(int i=0; i<myFirstArray.length; i++) {
+        for (int i = 0; i < myFirstArray.length; i++) {
             sum = sum + myFirstArray[i];
         }
-
+// minimum value of the array
         double min = myFirstArray[0];
-        for(int i=0; i<myFirstArray.length; i++) {
+        for (int i = 0; i < myFirstArray.length; i++) {
             if (myFirstArray[i] < min)
                 min = myFirstArray[i];
         }
