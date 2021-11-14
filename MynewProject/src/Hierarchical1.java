83691cae16469f39b44978a29625fb4234a2bf53
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 23:14:04 +0530

    Update Hierarchical1.java
    Corrected formatting

--- MynewProject/src/Hierarchical1.java
+++ MynewProject/src/Hierarchical1.java
@@ -1,14 +1,23 @@
 public class Hierarchical1 {
-    void eat (){System.out.println("eating...");}
+    void eat() {
+        System.out.println("eating...");
+    }
 }
-class Cat extends Hierarchical1{
-    void play(){System.out.println("playing...");}
+// create sub class cat of Hierarchical1
+class Cat extends Hierarchical1 {
+    void play() {
+        System.out.println("playing...");
+    }
 }
-class Kitten extends Hierarchical1{
-    void walk(){System.out.println("walking...");}
+// create sub class Kitten of Hierarchical1
+class Kitten extends Hierarchical1 {
+    void walk() {
+        System.out.println("walking...");
+    }
 }
-class Test2{
-    public static void main(String args[]){
+// Create a test class
+class Test2 {
+    public static void main(String args[]) {
         Kitten c = new Kitten();
         c.eat();
         c.walk();
