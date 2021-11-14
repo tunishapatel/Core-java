6aa669aec0c1c6a341fe7fbfcc5b1d08e8585262
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 23:10:54 +0530

    Update Handle.java
    Corrected formatting

--- MynewProject/src/Handle.java
+++ MynewProject/src/Handle.java
@@ -1,33 +1,28 @@
-class Handle{
+class Handle {
 
-    void div(int a, int b)throws ArithmeticException
-    {
-        if (b==0){
+    void div(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+// throw an arithmaticexception            
             throw new ArithmeticException();
-        }
-        else {
+        } else {
             int c;
-            c = a/b;
+            c = a / b;
             System.out.println(c);
         }
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Handle h = new Handle();
-
-        try{
-            h.div(20,0);
-        }
-
-        catch (ArithmeticException a) {
+//try, catch and final on arithmatic exception
+        try {
+            h.div(20, 0);
+        } catch (ArithmeticException a) {
             System.out.println("Can't divide");
-            int x=11,y=0,z;
-            z = x*y;
+            int x = 11, y = 0, z;
+            z = x * y;
             System.out.println(z);
-        }
-
-        finally {
-            int p=3,q=1,r;
+        } finally {
+            int p = 3, q = 1, r;
             r = p + q;
             System.out.println(r);
             System.out.println("Result");
