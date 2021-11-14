ba74f75dac7c17cc05507ce4df1e2b36ffe910e4
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 22:03:39 +0530

    Update A2.java
    Corrected formatting.

--- MynewProject/src/A2.java
+++ MynewProject/src/A2.java
@@ -1,19 +1,25 @@
 public abstract class A2 {
     abstract int getRateOfInterest();
 }
-    class SBI extends A2 {
-        int getRateOfInterest(){return 7;}
+// Create a class called SBI 
+class SBI extends A2 {
+    int getRateOfInterest() {
+        return 7;
     }
-    class PNB extends A2 {
-        int getRateOfInterest(){return 8;}
+}
+// Create a class called PNB 
+class PNB extends A2 {
+    int getRateOfInterest() {
+        return 8;
     }
-
-    class TestBank{
-        public static void main(String args[]){
-            A2  b;
-            b=new SBI();
-            System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");
-            b=new PNB();
-            System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");
-        }}
-
+}
+// Create a class called TestBank
+class TestBank {
+    public static void main(String args[]) {
+        A2 b;
+        b = new SBI();
+        System.out.println("Rate of Interest is: " + b.getRateOfInterest() + " %");
+        b = new PNB();
+        System.out.println("Rate of Interest is: " + b.getRateOfInterest() + " %");
+    }
+}. 
