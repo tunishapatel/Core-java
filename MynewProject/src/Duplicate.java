4f1a3bc383828308994d56921db5ea95c69ef749
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 22:59:53 +0530

    Update Duplicate.java

--- MynewProject/src/Duplicate.java
+++ MynewProject/src/Duplicate.java
@@ -6,7 +6,7 @@ public class Duplicate {
 
     public static void main (String args[]) {
         System.out.println("Java find and remove duplicates: ");
-        // create arraylist
+// create arraylist
         List < String > l = Arrays.asList("A", "B", "C", "D", "A", "C", "E", "E", "E");
         System.out.println(l.toString());
         Set < String > s = new LinkedHashSet < String > (l) ;
