ec59e425ffd7cfeb40c28805149d80ead85603c0
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 22:14:37 +0530

    Update Animals.java
    Corrected formatting

--- MynewProject/src/Animals.java
+++ MynewProject/src/Animals.java
@@ -6,24 +6,28 @@ public class Animals {
         System.out.println("Animals");
     }
 }
+// Create a sub class herbivores 
 
 class herbivores extends Animals {
     void eat() {
         System.out.println("herbivores eat plants");
     }
 }
+// Create a sub class omnivores
 
 class omnivores extends Animals {
     void eat() {
         System.out.println("omnivores eat plants and meat");
     }
 }
+// Create a sub class carnivores
 
 class carnivores extends Animals {
     void eat() {
         System.out.println("carnivores eat meat");
     }
 }
+// Create a class main
 
 class main {
     public static void main(String args[]) {
