f88e4304c66748b34b623bb27a04226b3e9babb1
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 22:56:25 +0530

    Update Drawable.java
    Corrected formatting

--- MynewProject/src/Drawable.java
+++ MynewProject/src/Drawable.java
@@ -2,12 +2,14 @@ interface Drawable {
 
     void Draw();
 }
-class A6 implements Drawable{
-    public void Draw(){System.out.println("Draw a Triangle.");}
+// Draw a triangle using interface
+class A6 implements Drawable {
+    public void Draw() {
+        System.out.println("Draw a Triangle.");
+    }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         A6 obj = new A6();
         obj.Draw();
     }
 }
-
