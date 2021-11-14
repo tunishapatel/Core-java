2a59749d22c27b113a1c954cf02ff423f17843e0
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 23:34:46 +0530

    Update hybridInheritance.java
    Corrected formatting

--- MynewProject/src/hybridInheritance.java
+++ MynewProject/src/hybridInheritance.java
@@ -3,36 +3,38 @@ public class hybridInheritance {
         System.out.println("HybridInheritance");
     }
 }
-    class GrandFather extends hybridInheritance {
-        public void show() {
-            System.out.println("I am a GrandFather.");
-        }
+// Create a subclass GrandFather of hybridInheritance
+class GrandFather extends hybridInheritance {
+    public void show() {
+        System.out.println("I am a GrandFather.");
     }
-
-    class Father extends GrandFather {
-        public void show() {
-            System.out.println("I am a Father.");
-        }
+}
+// Create a subclass Father of GrandFather
+class Father extends GrandFather {
+    public void show() {
+        System.out.println("I am a Father.");
     }
-
-    class Son extends Father {
-        public void show() {
-            System.out.println("I am a Son.");
-        }
+}
+// Create a subclass Son of Father
+class Son extends Father {
+    public void show() {
+        System.out.println("I am a Son.");
     }
-
-    class Daughter extends Father {
-        public void show() {
-            System.out.println("I am Daughter.");
-        }
+}
+// Create a subclass Daughter of Father
+class Daughter extends Father {
+    public void show() {
+        System.out.println("I am Daughter.");
     }
-    class TestHybrid {
-    public static void main(String args[]){
+}
+//Create a test class
+class TestHybrid {
+    public static void main(String args[]) {
 
         Daughter obj = new Daughter();
         obj.show();
 
     }
-    }
+}
 
 
