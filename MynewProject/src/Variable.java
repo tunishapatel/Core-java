c61f074db749fa78c344f1e145f75f31065fa021
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 23:28:38 +0530

    Update Variable.java
    Corrected formatting

--- MynewProject/src/Variable.java
+++ MynewProject/src/Variable.java
@@ -1,18 +1,17 @@
-public class Variable
-{
-
-    int var = 8;
-
+public class Variable {
+    int
+    var = 8;
 }
-
+// Create a subclass Numbers of Veriables
 class Number extends Variable {
 
-    int var = 10;
-    void display(){
+    int
+    var = 10;
+    void display() {
 
         System.out.println(super.var);
     }
-    public static void main (String args[]){
+    public static void main(String args[]) {
 
         Number num = new Number();
         num.display();
