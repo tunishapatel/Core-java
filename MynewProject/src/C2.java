edd089b5ec482a7707834df3b2be81c991094a64
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 22:46:46 +0530

    Update C2.java
    Corrected formatting

--- MynewProject/src/C2.java
+++ MynewProject/src/C2.java
@@ -1,24 +1,23 @@
 class C2 {
+ // sum of two integers       
+        int i, j, Sum;
+        C2() {
+        i = 1;
+        j = 5;
+        Sum = i + j;
+        System.out.println(" i= " + i);
+        System.out.println(" j= " + j);
+        System.out.println(" Sum= " + Sum);
+    }        
+    void display() {
+        System.out.println(i + " " + j + " " + Sum);
+    }
+    public static void main(String[] args) {
 
+        C2 obj = new C2();
+        obj.display();
 
-        int i,j,Sum;
-        C2(){
-            i = 1;
-            j = 5;
-            Sum = i + j;
-            System.out.println( " i= " +i);
-            System.out.println( " j= " +j);
-            System.out.println( " Sum= " +Sum);
-        }
-        void display(){
-            System.out.println(i+ " " +j+ " " +Sum);
-        }
-        public static void main(String[] args){
-
-            C2 obj = new C2();
-            obj.display();
-
-        }
+    }
 
 }
 
