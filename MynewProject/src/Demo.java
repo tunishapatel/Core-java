289e3e410ddb38236d6328e9b703a3b589ff7b8b
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 22:53:29 +0530

    Update Demo.java
    Corrected formatting

--- MynewProject/src/Demo.java
+++ MynewProject/src/Demo.java
@@ -2,19 +2,19 @@ public class Demo {
 
     static int x = 10;
     static int y;
-    static void func(int z){
-        System.out.println("x =" + x );
-        System.out.println("y =" +y);
-        System.out.println("z =" +z);
+    static void func(int z) {
+        System.out.println("x =" + x);
+        System.out.println("y =" + y);
+        System.out.println("z =" + z);
     }
-static {
-
+    //Running static initialize block
+    static {
         System.out.println("Running static initialize block ");
         y = x + 5;
-       }
-        public static void main (String args[]){
-            func(8);
-        }
+    }
+    public static void main(String args[]) {
+        func(8);
+    }
 
 }
 
