dd7e361ced55316114312eae4979abb1eccc4c96
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 23:21:20 +0530

    Update Statements.java
    Corrected formatting

--- MynewProject/src/Statements.java
+++ MynewProject/src/Statements.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Statements {
+public class Statements {
+    public static void main(String args[]) {
+        int x = 100;
+// If else statemeents for integer x
+        if (x < 20) {
+            System.out.println("The value of x is less than 20");
+
+        }
+
+        if (x == 10) {
+            System.out.println("The value of x is 10");
+
+        } else if (x < 10) {
+            System.out.println("The value of x is less than 10");
+
+        } else {
+            System.out.println("The value of x is more than 10");
+
+        }
+// Create a new veriable y and create if else statements for that      
+        int y = 50;
+        if (x == 100) {
+            if (y > 30)
+                System.out.println("x is 100 and y is more than 30");
+            else {
+                System.out.println("The values of x and y are ");
+            }
+        }
+    }
 }
