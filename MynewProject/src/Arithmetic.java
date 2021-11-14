3f8f9fefdab281282958d032a718f035512be2b2
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 22:21:40 +0530

    Update Arithmetic.java
    Corrected formatting

--- MynewProject/src/Arithmetic.java
+++ MynewProject/src/Arithmetic.java
@@ -2,20 +2,23 @@ package Polymorphism;
 
 public class Arithmetic {
     System.out.println("Example of compile-time polymorphism");
+// sum three integers
+    
     void sum(int i, int j) {
         int a = i + j;
         System.out.println("Sum of two integers: " + a);
     }
-
+// sum three integers
+    
     void sum(int i, int j, int k) {
         int b = i + j / k;
         System.out.println("Sum of three integers: " + b);
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Arithmetic Obj = new Arithmetic();
-        Obj.sum(34,45);
-        Obj.sum(23,20,5);
+        Obj.sum(34, 45);
+        Obj.sum(23, 20, 5);
     }
 }
 
