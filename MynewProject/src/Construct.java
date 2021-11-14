30d658d35f2faed62eb301b35c214f442f5ea78f
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 22:48:24 +0530

    Update Construct.java
    Corrected formatting

--- MynewProject/src/Construct.java
+++ MynewProject/src/Construct.java
@@ -1,15 +1,15 @@
 class Construct {
-    int i,j;
-    Construct(){
-        i=1;
-        j=2;
+    int i, j;
+// Create a constructor    
+    Construct() {
+        i = 1;
+        j = 2;
 
     }
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Construct obj = new Construct();
-        System.out.println(" " +obj.i);
-        System.out.println(" " +obj.j);
+        System.out.println(" " + obj.i);
+        System.out.println(" " + obj.j);
 
     }
 }
-
