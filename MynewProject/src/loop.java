3e162dac550f9df7faa325995ec8f4c10deb968f
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 23:39:05 +0530

    Update loop.java
    Corrected formatting

--- MynewProject/src/loop.java
+++ MynewProject/src/loop.java
@@ -1,22 +1,23 @@
 public class loop {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("for loop");
-        for(int x=1; x<11; x++){
+// Create for loop for integer x        
+        for (int x = 1; x < 11; x++) {
             System.out.println(x);
         }
+// Create while loop for integer y
         System.out.println("while loop");
-        int y=1;
-        while(y<20)
-        {
-        System.out.println(y);
-        y++;
-        }
-        System.out.println("do wile loop");
-
-        do{
+        int y = 1;
+        while (y < 20) {
             System.out.println(y);
             y++;
-        }while(y<30);
+        }
+// Create do while loop for integer y
+        System.out.println("do wile loop");
 
+        do {
+            System.out.println(y);
+            y++;
+        } while (y < 30);
     }
 }
