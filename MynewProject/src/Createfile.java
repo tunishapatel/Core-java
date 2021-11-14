0f7e3f9178b4d997c1a72de04253f0730eee180e
tunishapatel <92499844+tunishapatel@users.noreply.github.com>
2021-11-14 22:50:54 +0530

    Update Createfile.java
    Corrected formatting

--- MynewProject/src/Createfile.java
+++ MynewProject/src/Createfile.java
@@ -1,7 +1,7 @@
 package com.company;
 
-import java.io.File;  // Import the File class
-import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.File; // Import the File class
+import java.io.IOException; // Import the IOException class to handle errors
 
 public class Createfile {
 
